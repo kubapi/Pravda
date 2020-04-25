5de04d111c12661c5080272252cb78dc80f5da6c
kubapi <kuba.sliz@hotmail.com>
2020-04-25 17:38:52 +0200

    connecting with postgress

--- website/website/settings.py
+++ website/website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'newsapp.apps.NewsappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'newsapp',
+        'USER': 'postgres',
+        'PASSWORD': 'czarny_postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
