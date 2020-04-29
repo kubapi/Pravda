3c28665f86376cbc84d3f21f2f8546a1d536d7c0
kubapi <kuba.sliz@hotmail.com>
2020-04-30 00:46:32 +0200

    Starting Author authentitacion for editor

--- website/website/urls.py
+++ website/website/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('',include('newsapp.urls')),
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls'))
 ]
