04b88184498703ccafa92f40e3a61396fabf672d
kubapi <kuba.sliz@hotmail.com>
2020-07-10 22:20:39 +0200

    Inital commit for pravda_editor (cookiecutter)

--- scripts/translate.py
+++ scripts/translate.py
@@ -2,10 +2,4 @@ from googletrans import Translator
 
 def translate(text):
     translator = Translator()
-    return translator.translate(text, src='pl', dest='en').text
-
-
-Chcę żeby wykrył język tekstu i przetłumaczył na język angielski
-
-
-print(translate("Bossak znwou pijany. Świadkowie mówią, że był widziany z Trzaskowskim i Dudą w warszawskim klubie nocnym."))
+    return translator.translate(text, dest='en').text
