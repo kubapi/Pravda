17126d73d9b6f95901b5cc3ad245e4ab880160b7
kubapi <kuba.sliz@hotmail.com>
2020-10-14 22:37:29 +0200

    adding new model

--- pravda_platform/pravda_platform/pravda_brain/views.py
+++ pravda_platform/pravda_platform/pravda_brain/views.py
@@ -1,4 +1,18 @@
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+def results(request, question_id):
+    respone = "You are looking at the results of question %s."
+    return HttpResponse(respone % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
