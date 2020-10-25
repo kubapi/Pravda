e5d5e9f0c546e7ed76ba3654d00ccf0fe78ab823
kubapi <kuba.sliz@hotmail.com>
2020-10-25 15:39:14 +0100

    creating first models

--- platform/app/admin.py
+++ platform/app/admin.py
@@ -2,4 +2,13 @@
 
 from django.contrib import admin
 
+from .models import Author, Article, Claim, Site
+
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Article)
+admin.site.register(Claim)
+admin.site.register(Site)
+
+
+
--- platform/app/models.py
+++ platform/app/models.py
@@ -4,4 +4,51 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey('Author', on_delete=models.PROTECT)
 
+    pub_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-pub_date']
+
+class Claim(models.Model):
+    content = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.content
+
+class Site(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField()
+
+    WEBSITE = 'WEB'
+    FACEBOOK_PAGE = 'FBP'
+    FACEBOOK_ACCOUNT = 'FBA'
+    TWITTER = 'TWTRA'
+    OTHER  = 'O'
+
+    CATEGORY_CHOICES = (
+        (WEBSITE, "Website (common)"),
+        (FACEBOOK_PAGE,  "Facebook Page"),
+        (FACEBOOK_ACCOUNT, "Facebook Account"),
+        (TWITTER, "Twitter Account"),
+        (OTHER, "Other"),
+    )
+
+    category = models.CharField(max_length=40, choices = CATEGORY_CHOICES, default = WEBSITE)
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    number_of_published = models.IntegerField(max_length=200)
+
+    def __str__(self):
+        return self.name
