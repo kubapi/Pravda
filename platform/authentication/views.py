74039b04004581f3fad49f5c4e0110441ce9873d
kubapi <kuba.sliz@hotmail.com>
2020-10-25 12:13:11 +0100

    changing initial animation

--- platform/authentication/views.py
+++ platform/authentication/views.py
@@ -24,9 +24,9 @@ def login_view(request):
                 login(request, user)
                 return redirect("/")
             else:    
-                msg = 'Invalid credentials'    
+                msg = 'Nie mamy takich danych w systemie!'    
         else:
-            msg = 'Error validating the form'    
+            msg = 'Błąd formularza...'    
 
     return render(request, "accounts/login.html", {"form": form, "msg" : msg})
 
@@ -43,13 +43,13 @@ def register_user(request):
             raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
 
-            msg     = 'User created - please <a href="/login">login</a>.'
+            msg     = 'Konto stworzone - proszę <a href="/login">zaloguj się</a>.'
             success = True
             
             #return redirect("/login/")
 
         else:
-            msg = 'Form is not valid'    
+            msg = 'Błąd formularza...'    
     else:
         form = SignUpForm()
 
--- platform/core/urls.py
+++ platform/core/urls.py
@@ -1,11 +1,11 @@
 # -*- encoding: utf-8 -*-
 
 from django.contrib import admin
-from django.urls import path, include  # add this
+from django.urls import path, include  
 
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
     path('admin' , admin.site.urls),          # Django admin route 
     path("", include("authentication.urls")), # Auth routes - login / register
-    path("", include("app.urls"))             # UI Kits Html files
+    path("", include("app.urls"))             
 ]
