24b78af62d904230b882c3a8f17cca1e152b427c
kubapi <kuba.sliz@hotmail.com>
2020-12-26 13:36:30 +0100

    adding popups, background script, creating first design

--- browser_extension/scripts/content.js
+++ browser_extension/scripts/content.js
@@ -1,6 +1,13 @@
 console.log("Chrome extension go!");
 
-let paragraphs = document.getElementsByTagName("div");
-for (element of paragraphs) {
-    element.style['background-color'] = "red";
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse) {
+    console.log(message.txt);
+    if (message.txt == "hello") {
+        let paragraphs = document.getElementsByTagName('p');
+        for (elt of paragraphs) {
+            elt.style['background-color'] = 'red'
+        }
+    }
 }
